a95a856bef7660a33c5002b69bb8c43b3ae1d52f
marijokerud <mari.jokerud@gmail.com>
2023-01-13 10:12:36 +0100

    nmds update

--- 02_nmds.R
+++ 02_nmds.R
@@ -14,21 +14,11 @@ orditorp(pin_NMDS,display="sites",cex=1.2,air=0.01)
 
 #Calculate scores 
 site.scores <- as.data.frame(scores(pin_NMDS)$sites)  #Using the scores function from vegan to extract the site scores and convert to a data.frame
-site.scores$site <- rownames(site.scores)  # create a column of site names, from the rownames of data.scores
-site.scores <- site.scores %>% 
-  mutate(Artslinje_id = site) %>% 
-  mutate(Artslinje_id = gsub("_2015", "", as.character(site.scores$site))) %>% #remove _2015
-  mutate(Artslinje_id = gsub("_2018", "", as.character(site.scores$site))) %>% #remove _2018
-  mutate(Artslinje_id = gsub("_2015", "", as.character(site.scores$site))) %>% #remove _2021
 
-
-site.scores$site <- gsub("_2015", "", as.character(site.scores$site)) 
-site.scores$site <- gsub("_2018", "", as.character(site.scores$site)) 
-site.scores$site <- gsub("_2021", "", as.character(site.scores$site)) 
 
 #Plott=c(rep("Kystlynghei_G(F)",5), rep("Fastmatte_G",10), rep("Tue_G",10), rep("Kystlynghei_G",10), rep("Tue_L",10), rep("Fastmatte_L",10), rep("Kystlynghei_L",10), rep("Kystlynghei_G(F)",5) )
-data.scores$Plott <- Plott  #  add the plott variable created earlier
-head(data.scores)
+#data.scores$Plott <- Plott  #  add the plott variable created earlier
+#head(data.scores)
 
 
 #slått sammen hele artslinjer
