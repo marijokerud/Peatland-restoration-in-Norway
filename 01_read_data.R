fa1ef5172b754ec5bebe1318be4dad603d804afd
marijokerud <mari.jokerud@gmail.com>
2023-01-13 10:12:25 +0100

    site scores update

--- 01_read_data.R
+++ 01_read_data.R
@@ -5,10 +5,11 @@ artslinjer <- read_excel(path = "Data Kaldvassmyra.xlsx", sheet = "Data", col_na
 tv_verdi <- read_excel(path = "Data Kaldvassmyra.xlsx", sheet = "Ind.verdi_GAD_TV",range = "A1:L53" , col_names = TRUE)
 plassering <- read_excel(path = "Data Kaldvassmyra.xlsx", sheet = "Plassering", col_names = TRUE)
 
-# Data cleaning
+# DATA CLEANING
 library(tidyverse)
+library(labdsv)
 
-# Create community matrix every 10 m
+# COMMUNITY MATRIX every 10 m
 pinpoint_matrix<- artslinjer %>% 
   unite("community", Artslinje_id, AAR) %>% 
   select(community, Art) %>% 
@@ -16,12 +17,11 @@ pinpoint_matrix<- artslinjer %>%
   mutate(Abundance = 1) %>% 
   as.data.frame
 
-library(labdsv)
 pinpoint_mat<- matrify(pinpoint_matrix)
 pinpoint_mat <- pinpoint_mat %>% 
   select(-Litter, -litter, -dead_sph, -peat, -dead_wood)
 
-# Create community matrix per line
+# COMMUNITY MATRIX per line
 community_matrixK<- artslinjer %>% 
   unite("community", LINJE, AAR) %>% 
   select(community, Art) %>% 
@@ -29,8 +29,16 @@ community_matrixK<- artslinjer %>%
   mutate(Abundance = 1) %>% 
   as.data.frame
 
-
 com_matK<- matrify(community_matrixK) 
 
+plassering_short <- plassering %>% 
+  select(Artslinje_id, Meter_from_ditch)
 
-
+#SITE SCORES
+site.scores$site <- rownames(site.scores)  # create a column of site names, from the rownames of data.scores
+site.scores <- site.scores %>% 
+  mutate(Artslinje_id = site) %>% 
+  mutate(Artslinje_id = gsub("_2015", "", Artslinje_id)) %>% #remove _2015
+  mutate(Artslinje_id = gsub("_2018", "", Artslinje_id)) %>% #remove _2018
+  mutate(Artslinje_id = gsub("_2021", "", Artslinje_id)) %>%  #remove _2021
+  left_join(plassering_short)
