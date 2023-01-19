784c86d26a112a73876b30455ec18b968023b3ab
marijokerud <mari.jokerud@gmail.com>
2023-01-19 09:40:15 +0100

    added term + I(sqrt(Meter_from_ditch))

--- 03_Regression.R
+++ 03_Regression.R
@@ -1,37 +1,39 @@
-fit1.1 <- lm(NMDS1~ AAR+ Meter_from_ditch ,data = site.scores)
+
+fit1.1 <- lm(NMDS1~ AAR+ Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
 summary(fit1.1)
-plot(NMDS1~ AAR+ Meter_from_ditch ,data = site.scores)
-abline(fit1.1)
+par(mfrow = c(3, 2)) # This code put two plots in the same window
+plot(NMDS1~ AAR+ Meter_from_ditch ,data = point.scores)
+#abline(fit1.1)
 plot(fit1.1, which = 1)
-par(mfrow = c(1, 2)) # This code put two plots in the same window
 hist(fit1.1$residuals) # Histogram of residuals
 plot(fit1.1, which = 2)
 
-fit1.2 <- lm(NMDS1~ AAR* Meter_from_ditch ,data = site.scores)
+
+fit1.2 <- lm(NMDS1~ AAR* Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
 summary(fit1.2)
-plot(NMDS1~ AAR+ Meter_from_ditch ,data = site.scores)
-abline(fit1.2)
+par(mfrow = c(3, 2)) # This code put two plots in the same window
+plot(NMDS1~ AAR+ Meter_from_ditch ,data = point.scores)
+#abline(fit1.2)
 plot(fit1.2, which = 1)
-par(mfrow = c(1, 2)) # This code put two plots in the same window
 hist(fit1.2$residuals) # Histogram of residuals
 plot(fit1.2, which = 2)
 
 
-fit2.1 <- lm(NMDS2~ AAR+ Meter_from_ditch ,data = site.scores)
+fit2.1 <- lm(NMDS2~ AAR+ Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
 summary(fit2.1)
-plot(NMDS2~ AAR+ Meter_from_ditch, data = site.scores)
-abline(fit2.1)
+par(mfrow = c(3, 2)) # This code put two plots in the same window
+plot(NMDS2~ AAR+ Meter_from_ditch, data = point.scores)
+#abline(fit2.1)
 plot(fit2.1, which = 1)
-par(mfrow = c(1, 2)) # This code put two plots in the same window
 hist(fit2.1$residuals) # Histogram of residuals
 plot(fit2.1, which = 2)
 
 
-fit2.2 <- lm(NMDS2~ AAR*Meter_from_ditch ,data = site.scores)
+fit2.2 <- lm(NMDS2~ AAR*Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
 summary(fit2.2)
-plot(NMDS2~ AAR*Meter_from_ditch ,data = site.scores)
-abline(fit2.2)
+par(mfrow = c(3, 2)) # This code put two plots in the same window
+plot(NMDS2~ AAR*Meter_from_ditch ,data = point.scores)
+#abline(fit2.2)
 plot(fit2.2, which = 1)
-par(mfrow = c(1, 2)) # This code put two plots in the same window
 hist(fit2.2$residuals) # Histogram of residuals
 plot(fit2.2, which = 2)
