b8825e1b32b492799f7bea86cfaca4ff134f7735
marijokerud <mari.jokerud@gmail.com>
2023-01-26 15:18:39 +0100

    removing unused code

--- code/04_figures.R
+++ code/04_figures.R
@@ -28,15 +28,17 @@ ggplot(point.scores, aes(x= Meter_from_ditch, y=NMDS1)) +
 
 col= "810f7c" og "8856a7"
 
-mycolors <- c("#a6dba0", "#7b3294", "#c2a5cf", "#008837", "#f7f7f7")
+myfill <- c("#a6dba0", "#7b3294", "#c2a5cf", "#008837", "#f7f7f7")
+mycolors <- c("black", "black", "black", "black", "black")
+myshapes <- c(21, 22, 24)
 
 ### NMDS SITE AND SPECIES PLOT
-ggplot(data=all.point.scores,aes(x=NMDS1,y=NMDS2,)) + 
-  geom_point(aes(fill = linje), shape=21, size = 4) +
-  #geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
-  labs(x = "NMDS1 scores", y= "NMDS2 scores", colour = "Transect", fill = "Transect") +
-  #scale_color_manual(values=c("#253494", "#2c7fb8", "#41b6c4", "#a1dab4", "#ffffcc")) +
-  scale_fill_manual(values=mycolors) +
+ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = linje, color = linje, shape= AAR2),  size = 4) + # 
+  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Transect", color = "Transect", shape = "Year") + #,
+  scale_color_manual(values=mycolors) +
+  scale_fill_manual(values=myfill) +
+  scale_shape_manual(values = c(21,22,24)) +
   theme_bw() +
   theme(axis.title.x = element_text(size=14,hjust=0.5),
         axis.title.y = element_text(size=14,vjust=1),
@@ -46,9 +48,11 @@ ggplot(data=all.point.scores,aes(x=NMDS1,y=NMDS2,)) +
         legend.text = element_text(color="black", size=12)) +
   theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
         panel.grid.major.x=element_blank(),
-        #panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
         panel.grid.major.y=element_blank()) +
   geom_text_repel(aes(label=Artslinje_id), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
   geom_text_repel(data= species.scores, aes(x=NMDS1,y=NMDS2,label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 10))
 
-#save as 650x600
+##geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
+
+#save as 650x600 // 800x700
