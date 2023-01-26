d9d1687c3fdcee8e5ba777f4db74d2b7f0273464
marijokerud <mari.jokerud@gmail.com>
2023-01-26 15:18:10 +0100

    reorganizing

--- code/03_Regression.R
+++ code/03_Regression.R
@@ -1,6 +1,6 @@
 
 
-fit1.1 <- lm(NMDS1~ AAR + Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
+fit1.1 <- lm(NMDS1~ AAR * Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
 summary(fit1.1)
 par(mfrow = c(3, 2)) # This code put two plots in the same window
 plot(NMDS1~ AAR+ Meter_from_ditch ,data = point.scores)
@@ -10,7 +10,7 @@ hist(fit1.1$residuals) # Histogram of residuals
 plot(fit1.1, which = 2)
 
 
-fit1.2 <- lm(NMDS1~ AAR* Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
+fit1.2 <- lm(NMDS1~ AAR + Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
 summary(fit1.2)
 par(mfrow = c(3, 2)) # This code put two plots in the same window
 plot(NMDS1~ AAR+ Meter_from_ditch ,data = point.scores)
@@ -18,6 +18,19 @@ plot(NMDS1~ AAR+ Meter_from_ditch ,data = point.scores)
 plot(fit1.2, which = 1)
 hist(fit1.2$residuals) # Histogram of residuals
 plot(fit1.2, which = 2)
+
+anova(fit1.1, fit1.2)
+
+fit1.3 <- lm(NMDS1~ Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
+summary(fit1.3)
+par(mfrow = c(3, 2)) # This code put two plots in the same window
+plot(NMDS1~ AAR+ Meter_from_ditch ,data = point.scores)
+#abline(fit1.3)
+plot(fit1.3, which = 1)
+hist(fit1.3$residuals) # Histogram of residuals
+plot(fit1.3, which = 2)
+
+anova(fit1.2, fit1.3)
 
 
 fit2.1 <- lm(NMDS2~ AAR+ Meter_from_ditch + I(sqrt(Meter_from_ditch)), data = point.scores)
