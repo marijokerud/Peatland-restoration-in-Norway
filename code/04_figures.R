66559e7bc2759a9b9fd5cdf21b38ff7e520db46f
marijokerud <mari.jokerud@gmail.com>
2023-01-30 14:02:55 +0100

    wesanderson colors :)

--- code/04_figures.R
+++ code/04_figures.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(ggrepel)
+library(wesanderson)
 
 ### POLYNOMIAL REGRESSION PLOT
 ggplot(point.scores, aes(x= Meter_from_ditch, y=NMDS1)) +
@@ -26,7 +27,7 @@ ggplot(point.scores, aes(x= Meter_from_ditch, y=NMDS1)) +
 
 #save as 575x650
 
-col= "810f7c" og "8856a7"
+#col= "810f7c" og "8856a7"
 
 myfill <- c("#a6dba0", "#7b3294", "#c2a5cf", "#008837", "#f7f7f7")
 mycolors <- c("black", "black", "black", "black", "black")
@@ -37,7 +38,8 @@ ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) +
   geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = linje, color = linje, shape= AAR2),  size = 4) + # 
   labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Transect", color = "Transect", shape = "Year") + #,
   scale_color_manual(values=mycolors) +
-  scale_fill_manual(values=myfill) +
+  #scale_fill_manual(values=myfill) +
+  scale_fill_manual(values=wes_palette(name="Darjeeling2", n=5)) +
   scale_shape_manual(values = c(21,22,24)) +
   theme_bw() +
   theme(axis.title.x = element_text(size=14,hjust=0.5),
@@ -51,7 +53,7 @@ ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) +
         panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
         panel.grid.major.y=element_blank()) +
   geom_text_repel(aes(label=Artslinje_id), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
-  geom_text_repel(data= species.scores, aes(x=NMDS1,y=NMDS2,label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 10))
+  geom_text_repel(data= species.scores, aes(x=NMDS1,y=NMDS2,label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
 
 ##geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
 
