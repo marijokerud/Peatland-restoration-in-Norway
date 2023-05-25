a12db03bd3970a85983b61ab9c4a8e17cd2c4354
marijokerud <mari.jokerud@gmail.com>
2023-05-25 23:38:20 +0200

    updated for Hildremsvatnet

--- code/04_figures.R
+++ code/04_figures.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(ggrepel)
 library(wesanderson)
 
+#KALDVASSMYRA
 ### POLYNOMIAL REGRESSION PLOT
 ggplot(point.scores, aes(x= Meter_from_ditch, y=NMDS1)) +
   geom_point(shape=1, color= "black", size=2.2) +
@@ -52,7 +53,37 @@ ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) +
         panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
         panel.grid.major.y=element_blank()) +
   geom_text_repel(aes(label=linje), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
-  geom_text_repel(data= species.scores, aes(x=NMDS1,y=NMDS2,label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
+  geom_text_repel(data= species.scores, aes(x=NMDS1, y=NMDS2, label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
+
+##geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
+
+#save as 650x600 // 800x700
+
+#HILDREMSVATNET
+
+myfill <- c("#f0f9e8", "#ccebc5", "#43a2ca", "#0868ac") # "#a8ddb5", "#7bccc4",
+mycolors <- c("black", "black", "black", "black")
+
+### NMDS SITE AND SPECIES PLOT
+ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, color = linje, fill = linje, shape = AAR2), size = 4) +  
+  labs(x = "NMDS1 scores", y= "NMDS2 scores") + 
+  scale_color_manual(values=myfill, name="Transect") +
+  scale_fill_manual(values=myfill, name="Transect") +
+  scale_shape_manual(values = c(21,22), labels=c("Before","Two years after"), name="Restoration") +
+  theme_bw() +
+  theme(axis.title.x = element_text(size=14,hjust=0.5),
+        axis.title.y = element_text(size=14,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text(color="black", size=14),
+        legend.text = element_text(color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.major.y=element_blank()) +
+  geom_text_repel(aes(label=linje), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
+  geom_text_repel(data= species.scores, aes(x=NMDS1, y=NMDS2, label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
 
 ##geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
 
