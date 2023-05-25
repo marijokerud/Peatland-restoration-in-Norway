3bd35400c9b6a672f0f86806ab5a53b9809cb952
marijokerud <mari.jokerud@gmail.com>
2023-05-25 09:38:38 +0200

    change colors and labels

--- code/04_figures.R
+++ code/04_figures.R
@@ -29,18 +29,17 @@ ggplot(point.scores, aes(x= Meter_from_ditch, y=NMDS1)) +
 
 #col= "810f7c" og "8856a7"
 
-myfill <- c("#a6dba0", "#7b3294", "#c2a5cf", "#008837", "#f7f7f7")
-mycolors <- c("black", "black", "black", "black", "black")
+myfill <- c("#f0f9e8", "#ccebc5", "#a8ddb5", "#7bccc4", "#43a2ca", "#0868ac", "black")
+mycolors <- c("black", "black", "black", "black", "black", "black", "black")
 myshapes <- c(21, 22, 24)
 
 ### NMDS SITE AND SPECIES PLOT
 ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
-  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = linje, color = linje, shape= AAR2),  size = 4) + # 
-  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Transect", color = "Transect", shape = "Year") + #,
-  scale_color_manual(values=mycolors) +
-  #scale_fill_manual(values=myfill) +
-  scale_fill_manual(values=wes_palette(name="Darjeeling2", n=5)) +
-  scale_shape_manual(values = c(21,22,24)) +
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = as.factor(Meter_from_ditch), color = as.factor(Meter_from_ditch), shape= AAR2),  size = 4) + # 
+  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Distance from ditch", color = "Distance from ditch", shape = "Year") + #,
+  scale_color_manual(values=myfill) +
+  scale_fill_manual(values=myfill) +
+  scale_shape_manual(values = c(21,22,24), labels=c("Before","Two years after", "Five years after"),name="Restoration") +
   theme_bw() +
   theme(axis.title.x = element_text(size=14,hjust=0.5),
         axis.title.y = element_text(size=14,vjust=1),
@@ -52,7 +51,7 @@ ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) +
         panel.grid.major.x=element_blank(),
         panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
         panel.grid.major.y=element_blank()) +
-  geom_text_repel(aes(label=Artslinje_id), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
+  geom_text_repel(aes(label=linje), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
   geom_text_repel(data= species.scores, aes(x=NMDS1,y=NMDS2,label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
 
 ##geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
