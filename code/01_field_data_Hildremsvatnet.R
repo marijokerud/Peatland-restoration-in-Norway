6974a3a46c4e195508232c76b3a6986f3187cc5a
marijokerud <mari.jokerud@gmail.com>
2023-12-01 10:05:32 +0100

    cleaning up repo and codes

---
+++ code/01_field_data_Hildremsvatnet.R
@@ -0,0 +1,52 @@
+library(readxl)
+library(tidyverse)
+library(labdsv)
+
+
+feltskjema_kaldvassmyra <- read_excel(path = "data/Data_myr_restaurering.xlsx", sheet = "Kaldvassmyra2023", col_names = TRUE)
+feltskjema_aurstadmosan <- read_excel(path = "data/Data_myr_restaurering.xlsx", sheet = "Aurstadmåsan2023", col_names = TRUE)
+feltskjema_trysil <- read_excel(path = "data/Data_myr_restaurering.xlsx", sheet = "Trysil2023", col_names = TRUE)
+
+# DATA CLEANING
+
+#ÅURSTADMÅSAN
+aurstadmosan2023 <- feltskjema_aurstadmosan %>%
+  gather("cm", "presence", -year, -site, -field_analyst, -transect_id, -species) %>% 
+  mutate(site = gsub("Aurstadmasan", "Aurstadmosen", site)) %>%
+  mutate(cm = as.numeric(cm)) %>% 
+  drop_na() %>% 
+  select(-presence, -field_analyst) %>% 
+  rename(AAR = year, OMRADE = site, Artslinje_id = transect_id, Art = species)
+
+
+#KALDVASSMYRA
+kaldvassmyra2023 <- feltskjema_kaldvassmyra %>%
+  gather("CM", "presence", -year, -site, -field_analyst, -transect_id, -species) %>% 
+  mutate("cm"= paste(CM, 0, sep = "")) %>% 
+  mutate(cm = as.numeric(cm)) %>% 
+  mutate(species = gsub("Betula nana B.", "Betula nana", species)) %>% 
+  mutate(species = gsub("Betula nana C.", "Betula nana", species)) %>%
+  mutate(species = gsub("Betula nana F.", "Betula nana", species)) %>%
+  mutate(species = gsub("Picea abies B.", "Picea abies", species)) %>%
+  mutate(species = gsub("Pinus sylvestris B.", "Pinus sylvestris", species)) %>%
+  mutate(species = gsub("Pinus sylvestris C.", "Pinus sylvestris", species)) %>%
+  mutate(AAR2 = 6) %>% 
+  mutate(Treatment = "after3") %>% 
+  drop_na() %>% 
+  select(-CM, -presence, -field_analyst) %>% 
+  rename(AAR = year, OMRADE = site, Artslinje_id = transect_id, Art = species)
+
+
+#TRYSIL
+trysil2023 <- feltskjema_trysil %>%
+  gather("cm", "presence", -transect_id, -species) %>% 
+  mutate(presence = as.numeric(gsub("x", 1, presence))) %>% 
+  drop_na() %>% 
+  select(-presence) %>% 
+  mutate(AAR = as.numeric("2023")) %>% 
+  mutate(OMRADE = "Hisåsen Regnåsen") %>%
+  mutate(cm = as.numeric(cm)) %>%
+  rename(Artslinje_id = transect_id, Art = species) %>% 
+  select(AAR, OMRADE, Artslinje_id, Art, cm)
+
+  
---
+++ code/04_figure_Kaldvassmyra.R
@@ -0,0 +1,146 @@
+library(ggplot2)
+library(ggrepel)
+#library(wesanderson)
+
+#Referance line confidence interval
+R5 <- all.point.scores %>% 
+  slice(70:74) 
+modelR<- lm(NMDS1~1, R5)
+confint(modelR, level = 0.95)
+
+R5 %>% summarise(avg = mean(NMDS1))
+
+#KALDVASSMYRA
+### POLYNOMIAL REGRESSION PLOT
+ggplot(all.point.scores, aes(x= Meter_from_ditch, y=NMDS1)) +
+  geom_point(shape=1, color= "black", size=2.2) +
+  geom_smooth(method='lm', formula= y~x+sqrt(x), se=FALSE, col="black", lwd=1.2) +
+  geom_hline(aes(yintercept = -0.3732912), linetype = "dotted", col = "#e08214") +
+  labs(x = "Meters from ditch", y= "NMDS1 scores") +
+  theme_bw() +
+  annotate(geom = "rect", xmin= 0, xmax= 51, ymin= -0.9631129, ymax= 0.2165305, fill="#e08214", alpha=0.4) +
+  theme(axis.title.x = element_text(size=14,hjust=0.5),
+        axis.title.y = element_text(size=14,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text(color="black", size=14),
+        legend.text = element_text(color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
+        panel.grid.major.x=element_blank(),
+        #panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.major.y=element_blank()) 
+
+#save as 575x650
+
+#myfill <- c("#7b3294","#008837","#c2a5cf","#a6dba0","#e7d4e8","#d9f0d3","#e08214")
+#myfill <- c("#762a83","#1b7837","#af8dc3","#7fbf7b","#e7d4e8","#d9f0d3","#e08214")
+myfill <- c("#762a83","#1b7837","#9970ab","#5aae61","#c2a5cf","#a6dba0","#e08214")
+
+### NMDS SITE AND SPECIES PLOT
+ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = as.factor(Meter_from_ditch), color = as.factor(Meter_from_ditch), shape= AAR2),  size = 4) + # 
+  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Distance from ditch", color = "Distance from ditch", shape = "Year") + #,
+  scale_color_manual(values=myfill) +
+  scale_fill_manual(values=myfill) +
+  scale_shape_manual(values = c(21,22,24), labels=c("Before","Two years after", "Five years after"),name="Restoration") +
+  theme_bw() +
+  theme(axis.title.x = element_text(size=14,hjust=0.5),
+        axis.title.y = element_text(size=14,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text(color="black", size=14),
+        legend.text = element_text(color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.major.y=element_blank()) +
+  geom_text_repel(data= species.scores, aes(x=NMDS1, y=NMDS2, label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
+
+#geom_text_repel(aes(label=linje), size=2.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 5)) +  #add smaller text
+#geom_polygon(data=all.point.scores, aes(x = NMDS1, y = NMDS2, fill= linje, group=linje), alpha = 0.30) +
+
+#save as 650x600 // 800x700
+
+#Distance from ditch
+myfill <- c("#762a83","#1b7837","#9970ab","#5aae61","#c2a5cf","#a6dba0","#e08214")
+distance<- 
+  ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = as.factor(Meter_from_ditch), color = as.factor(Meter_from_ditch)),  size = 4) + # 
+  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Distance from ditch", color = "Distance from ditch") + #,
+  scale_color_manual(values=myfill) +
+  scale_fill_manual(values=myfill) +
+  theme_bw() +
+  theme(axis.title.x = element_text(size=14,hjust=0.5),
+        axis.title.y = element_text(size=14,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text(color="black", size=14),
+        legend.text = element_text(color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.major.y=element_blank()) +
+  geom_text_repel(data= species.scores, aes(x=NMDS1, y=NMDS2, label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
+#save as 650x600 // 800x700
+
+#YEAR
+myfill <- c("#998ec3","#d8b365","#5ab4ac")
+year<- 
+  ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, fill = AAR, color = AAR, shape= AAR),  size = 4) + # 
+  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Year", color = "Year", shape = "Year") + #,
+  scale_color_manual(values=myfill, labels=c("","", ""),name="") +
+  scale_fill_manual(values=myfill, labels=c("","", ""),name="") +
+  scale_shape_manual(values = c(21,22,24), labels=c("Before","Two years after", "Five years after"),name="Restoration") +
+  theme_bw() +
+  theme(axis.title.x = element_text(size=14,hjust=0.5),
+        axis.title.y = element_text(size=14,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text(color="black", size=14),
+        legend.text = element_text(color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.major.y=element_blank()) +
+  geom_text_repel(data= species.scores, aes(x=NMDS1, y=NMDS2, label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
+#save as 650x600 // 800x700
+
+
+library(cowplot)
+
+ggdraw() +
+draw_plot(distance, 0, 0, .5) +
+draw_plot(year, .5, 0, .5) +
+draw_plot_label(c("(a)", "(b)"), c(0, .5), c(1, 1), hjust = -0.2,  size = 15)
+#save as 1400x700
+
+
+
+
+#HILDREMSVATNET
+
+myfill <- c("#d8b365","#5ab4ac")
+
+### NMDS SITE AND SPECIES PLOT
+ggplot(data=all.point.scores, aes(x=NMDS1, y=NMDS2)) + 
+  geom_point(data=all.point.scores, aes(x=NMDS1, y=NMDS2, color = AAR2, fill = AAR2, shape = linje), size = 4) +  
+  labs(x = "NMDS1 scores", y= "NMDS2 scores", fill = "Year", color = "Year", shape = "Species line") + #,
+  scale_color_manual(values=myfill, labels=c("Before","Two years after"),name="Restoration") +
+  scale_fill_manual(values=myfill, labels=c("Before","Two years after"),name="Restoration") +
+  scale_shape_manual(values = c(21,22,23,24)) +
+  theme_bw() +
+  theme(axis.title.x = element_text(size=14,hjust=0.5),
+        axis.title.y = element_text(size=14,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text(color="black", size=14),
+        legend.text = element_text(color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),                          #Hide all the vertical gridlines
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.y=element_blank(),                           #Hide all the horizontal gridlines
+        panel.grid.major.y=element_blank()) +
+  geom_text_repel(data= species.scores, aes(x=NMDS1, y=NMDS2, label=species), size=3.5, alpha=0.5, max.overlaps = getOption("ggrepel.max.overlaps", default = 15))
+
+
+#save as 650x600 // 800x700
